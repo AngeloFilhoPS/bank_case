66ada704691a8b2c00275c2c763aa705a9354c91
AngeloFilhoPS <angelo.marcio.dev@gmail.com>
2024-10-09 22:30:29 -0400

    feat: open api config and test

--- src/main/java/pismo/spring/bank_api/account/AccountController.java
+++ src/main/java/pismo/spring/bank_api/account/AccountController.java
@@ -1,6 +1,8 @@
 package pismo.spring.bank_api.account;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,11 +14,12 @@ public class AccountController {
     private final AccountService service;
 
     @PostMapping
+    @Operation(summary = "Cria uma nova conta", description = "Cria uma nova conta a partir de um número de documento.")
     public ResponseEntity<AccountResponseDTO> createAccount(
             @RequestBody AccountCreateRequestDTO requestDTO){
         AccountResponseDTO createdAccount = service.createAccountByDocument(requestDTO);
         return ResponseEntity
-                .ok()
+                .status(HttpStatus.CREATED)
                 .body(createdAccount);
     }
 
---
+++ src/main/java/pismo/spring/bank_api/core/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package pismo.spring.bank_api.core.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Bank Case - Financial Account")
+                        .version("1.0")
+                        .description("Projeto de case para entrevista de emprego"));
+    }
+}
